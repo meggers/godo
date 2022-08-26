1ee55f990b87ac09f468a6611a76a909ca3052de
Max Eggers <eggers.max@gmail.com>
2022-08-26 14:19:09 +0000

    fix: update todoitem test for removed id

--- internal/godo/todoItem_test.go
+++ internal/godo/todoItem_test.go
@@ -3,7 +3,7 @@ package godo
 import "testing"
 
 func TestParseDescriptionOnly(t *testing.T) {
-	got := NewTodoItem(1, "a description")
+	got := NewTodoItem("a description")
 
 	if got.Description != "a description" {
 		t.Errorf("Abs(a description) = %v; want a description", got.Description)
